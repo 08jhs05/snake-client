f8b7c2395f9296ea22cbc5ae4e295dd14bef3b78
JayShin <08jhs05@gmail.com>
2021-04-07 15:04:12 -0400

    terminate the game when ctrl c is pressed

--- play.js
+++ play.js
@@ -1,22 +1,23 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function(key){
+  if(key === '\u0003'){
+    process.exit();
+  }
+  console.log(key)
+}
+
+setupInput();
